f1e46302f5c239a7f72ef5a0c943812c3935f359
SamuelReginaldo <usr1@gmail.com>
2023-09-15 17:27:01 +0200

    llista nums

--- Hello.java
+++ Hello.java
@@ -1,5 +1,9 @@
 public class Hello {
     public static void main(String[]){
         println.nextLine("Hello");
+
+        for(int i=0; i<5; i++){
+            System.out.println(i);
+        }
     }
 }
