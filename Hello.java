ddef805823a42695995e65ab0c2ddd49eb56251e
SamuelReginaldo <usr1@gmail.com>
2023-09-15 18:36:50 +0200

    compte enrera

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String[]){
         println.nextLine("Hello");
+
+        int count = 3;
+        while(count>3){
+            System.out.println("Compte enrera"+ count);
+            count--;
+        }
     }
 }
